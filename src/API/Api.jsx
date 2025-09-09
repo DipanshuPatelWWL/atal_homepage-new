1162e143244824169566135c336e34c036935d97
Ruchi <ruchi.wwl@gmail.com>
2025-09-09 13:57:18 +0530

    merge 2 R code

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://atal-backend.onrender.com",
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://atal-backend.onrender.com/api",
+  // baseURL: "http://localhost:4000/api",
   withCredentials: true,
 })
 
